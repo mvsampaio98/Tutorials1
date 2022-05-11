692c90c7d1c99aa3fa65c7183f2999e5dd4c8dc9
Marcelo Sampaio <mvsampaio98@gmail.com>
2022-05-11 14:31:37 -0300

    Update Ford.py

---
+++ Ford.py
@@ -0,0 +1,13 @@
+#Importing librabries
+import pandas as pd
+import numpy as np
+import seaborn as sns
+#Search Dataset path
+data_filepath = 'C:/Users/Marcelo Sampaio/Desktop/Datasets/ford.csv'
+my_data = pd.read_csv(data_filepath)
+my_data.head()
+#Transform U$ in R$ and Mileage in Kilometers
+my_data['price'] = my_data['price'] * 5.11
+my_data['mileage'] = my_data['mileage'] * 1.61
+my_data.head()
+my_data.rename(columns={'price':'price_R$', 'mileage':'kilometers'})
